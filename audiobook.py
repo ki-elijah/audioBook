54889c6f21757124ad6e48a1e8da4ef3cb624983
ki-elijah <elijahknsubuga@gmail.com>
2021-09-13 12:29:38 +0300

    changed some text

--- audiobook.py
+++ audiobook.py
@@ -1,11 +1,11 @@
 import PyPDF2
 
-pdfReader = PyPDF2.PdfFileReader(
-    open('The life of A W Tozer  In Pursuit of God.pdf', 'rb'))
+pdfReader = PyPDF2.PdfFileReader(open('file.pdf', 'rb'))
 
 import pyttsx3
 
 speaker = pyttsx3.init()
+
 for pagenum in range(pdfReader.numPages):
     text = pdfReader.getPage(pagenum).extractText()
     speaker.say(text)
